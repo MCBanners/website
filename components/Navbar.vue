219fcbc0c69d9d844b8dd191885d5794bff5d87a
darbyjack <admin@glaremasters.me>
2019-12-15 00:54:36 -0600

    Added a navbar to the default layout

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-navbar toggleable="sm" type="light" variant="light">
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+      <b-navbar-brand>MCBanners</b-navbar-brand>
+
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Spigot</b-nav-item>
+          <b-nav-item href="#">Servers</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <navbar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Navbar from '~/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 html {
