0f6290a35b1385f0592060036bfe2603a867fdd3
darbyjack <admin@glaremasters.me>
2019-12-15 00:59:05 -0600

    Updated subtitle.

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         MCBanners
       </h1>
       <h2 class="subtitle">
-        MCBanners
+        Spicing up showcases for projects has never been easier.
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
