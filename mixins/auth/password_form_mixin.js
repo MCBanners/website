7599f076648722bbce95820ff57a675b22e56393
simpleauthority <jacob@algorithmjunkie.com>
2020-01-11 01:35:02 -0800

    Needs to be less than, not equality

--- mixins/auth/password_form_mixin.js
+++ mixins/auth/password_form_mixin.js
@@ -6,7 +6,7 @@ export default {
   },
   computed: {
     passwordRequirementsNotMet() {
-      return this.password.length !== 8
+      return this.password.length < 8
     },
     passwordOk() {
       if (!this.password) return null
